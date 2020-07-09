0bb4d14ebdb9cbf6ae99c07e6cb133178a360a8d
dungtn911 <hoangtudauphong@gmail.com>
2020-07-09 08:07:37 -0700

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Richard Pham on 7/9/20.
+//  Copyright © 2020 Richard Pham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
